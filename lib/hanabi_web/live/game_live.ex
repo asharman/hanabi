37123868454456fcdcdb9d165d5f88f7c642c4ea
Austin Sharman <asharman22@gmail.com>
2022-01-14 21:10:28 -0500

    Add liveview for lobby

---
+++ lib/hanabi_web/live/game_live.ex
@@ -0,0 +1,40 @@
+defmodule HanabiWeb.GameLive do
+  # If you generated an app with mix phx.new --live,
+  # the line below would be: use MyAppWeb, :live_view
+  use HanabiWeb, :live_view
+  import Ecto.Changeset
+  @form_types %{username: :string}
+
+  def mount(%{"name" => name}, _session, socket) do
+    Hanabi.start_lobby(name)
+    players = Hanabi.lobby_players(name)
+
+    changeset = change({%{}, @form_types})
+
+    socket =
+      socket
+      |> assign(:players, players)
+      |> assign(:name, name)
+      |> assign(:changeset, changeset)
+
+    {:ok, socket}
+  end
+
+  def handle_event("add_player", %{"player" => params}, socket) do
+    with {:ok, %{username: new_player}} <- cast_params(params, @form_types),
+         :ok <- Hanabi.add_player_to_lobby(socket.assigns.name, new_player) do
+      players = Hanabi.lobby_players(socket.assigns.name)
+      {:noreply, assign(socket, :players, players)}
+    else
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
+  end
+
+  defp cast_params(form_data, type_map) do
+    {%{}, type_map}
+    |> cast(form_data, Map.keys(type_map))
+    |> validate_required([:username])
+    |> apply_action(:update)
+  end
+end
--- lib/hanabi_web/router.ex
+++ lib/hanabi_web/router.ex
@@ -18,6 +18,7 @@ defmodule HanabiWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    live "/game/:name", GameLive
   end
 
   # Other scopes may use custom stacks.
