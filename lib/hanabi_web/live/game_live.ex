b652aeb0bdbbe3ad9effddd12bbddde6266ce640
Austin Sharman <asharman22@gmail.com>
2022-01-14 21:45:34 -0500

    Broadcast PubSub message when player joins lobby

--- lib/hanabi_web/live/game_live.ex
+++ lib/hanabi_web/live/game_live.ex
@@ -6,6 +6,10 @@ defmodule HanabiWeb.GameLive do
   @form_types %{username: :string}
 
   def mount(%{"name" => name}, _session, socket) do
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(Hanabi.PubSub, "lobby:#{name}")
+    end
+
     Hanabi.start_lobby(name)
     players = Hanabi.lobby_players(name)
 
@@ -23,12 +27,18 @@ defmodule HanabiWeb.GameLive do
   def handle_event("add_player", %{"player" => params}, socket) do
     with {:ok, %{username: new_player}} <- cast_params(params, @form_types),
          :ok <- Hanabi.add_player_to_lobby(socket.assigns.name, new_player) do
-      players = Hanabi.lobby_players(socket.assigns.name)
-      {:noreply, assign(socket, :players, players)}
+      Phoenix.PubSub.broadcast(Hanabi.PubSub, "lobby:#{socket.assigns.name}", :new_player)
+
+      {:noreply, socket}
     else
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, :changeset, changeset)}
     end
+  end
+
+  def handle_info(:new_player, socket) do
+    players = Hanabi.lobby_players(socket.assigns.name)
+    {:noreply, assign(socket, :players, players)}
   end
 
   defp cast_params(form_data, type_map) do
